ba2eec668b960b2ffa2e310d7adcc6d588e08caa
omarmorad <omorad5362@gmail.com>
2025-10-08 16:34:35 +0300

    register method for the auth controller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends BaseController
+{
+    public function register(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'max:255'],
+            ]);
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage(), null, 500);
+        }
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->success([
+            'token' => $token,
+        ], 'User registered successfully', 201);
+    }
+}
