a97555e31761621b33c86ecf079a5192ef3b1ca2
omarmorad <omorad5362@gmail.com>
2025-10-12 11:19:19 +0300

    login route

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register' , [AuthController::class, 'register']);
+
+
+
+Route::post('/login', [AuthController::class, 'login']);
