af73b5f67ab3387309801842f4d7900d8e33b5fd
omarmorad <omorad5362@gmail.com>
2025-10-12 12:41:36 +0300

    refactor logout route to use middleware for authentication

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'Logout']);
+});
 Route::post('/register' , [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
