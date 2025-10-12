c6939425233a92ecd933f301d535c512ab67c894
omarmorad <omorad5362@gmail.com>
2025-10-12 11:29:07 +0300

    logout route

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register' , [AuthController::class, 'register']);
-
-
-
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
