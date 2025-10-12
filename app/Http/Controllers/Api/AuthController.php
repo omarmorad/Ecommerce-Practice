9069086866be5339a2f45e4b0c305fe6b2430b32
omarmorad <omorad5362@gmail.com>
2025-10-12 11:19:13 +0300

    add login functionality to auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use PHPUnit\TextUI\XmlConfiguration\RemoveCoverageElementCacheDirectoryAttribute;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends BaseController
 {
@@ -32,5 +35,39 @@ class AuthController extends BaseController
         return $this->success([
             'token' => $token,
         ], 'User registered successfully', 201);
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => ['required', 'string', 'email'],
+                'password' => ['required', 'string'],
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'errors' => $e->errors(),
+            ], 422);
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Login Successful',
+            'user' => $user,
+            'token' => $token,
+
+
+
+        ]);
+
     }
 }
